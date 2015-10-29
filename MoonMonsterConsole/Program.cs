2248733830b38c1b3143c48b462a89d8dca09141
antleypk <peterantley@gmail.com>
2015-10-29 12:54:36 -0400

    here is my most up to date commit, still having trouble with the monster class

--- MoonMonsterConsole/Program.cs
+++ MoonMonsterConsole/Program.cs
@@ -4,10 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+
+
+
 namespace MoonMonsterConsole
 {
+    //first and foremost, this is amazing to finally be at this stage, if you are reading this, you are a winner
+    // we have a couple goals for what needs to be happen next, first and foremost the math that will be used
+    // in this program comes from the google doc hippo folder, inside hippo>monster catcher>Math calculations. 
+    // the main function should just be here for calling pvp battle
+    // our first goals are this, 1 get the monster class to interact with the main
+    // 2 we need to create a move class
+    // we need to make a file for stock moves and creatures
+    // we need to make files to hold creatures for programmers, so that each programmer can make a roseter and keep creatures on the roster, and fight other matches
+    // we need to make a class that has the input of two rosters, then allows two people to engage in a console based creature match
     class Program
     {
+        
         /// <summary>
         /// test
         /// this is working
@@ -17,7 +31,7 @@ namespace MoonMonsterConsole
 
 
         {
-            
+           
      
         }
     }
---
+++ monster.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class monster
+{
+
+    // The first variable is a test
+    int firstInt = 0;
+    // the first set of variables are the ones that will change
+    int id = 0;
+    int health = 0;
+    int stamina = 0;
+    int level = 0;
+    // the second list is variables that will remain constant unless an evolution/item is used. 
+    int speed = 0;
+    int agility = 0;
+    int strenght = 0;
+
+    // this will be a list of moves that are available to the creature
+    List<int> moveList = new List<int>();
+
+
+    // here I am attempting to intialize the monster and give it a test value, i then expect this function to assign the value and print firstInt
+    public monster(int start)
+    {
+        firstInt = start;
+        print();
+
+    }
+    
+	void print()
+    {
+        Console.Write("first int= "+firstInt);
+    }
+	
+}
